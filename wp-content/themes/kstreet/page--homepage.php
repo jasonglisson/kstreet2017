6b47e09e1db0b5a1732cfa6dd6edc36444469d25
Jason Glisson <jasonglisson@Jasons-MacBook-Air.local>
2017-06-23 08:04:34 -0400

    Added process icons

--- wp-content/themes/kstreet/page--homepage.php
+++ wp-content/themes/kstreet/page--homepage.php
@@ -23,20 +23,20 @@ Template Name: Homepage
 </div>	
 <div class="process">
   <div class="row">
-    <h2 class="large-12 columns">Investment Process</h2>
+    <h1 class="large-12 columns">Investment Process</h1>
     <div classs="large-12 columns">
       <div class="process-wrap">
-        <div class="round-step step-1">icon</div>
+        <div class="round-step step-1"><img src="<?php print get_template_directory_uri(); ?>/library/images/application-icon.png" alt="Application" title="Application"/></div>
         <div class="process-line line-1"></div>
-        <div class="round-step step-2">icon</div>
+        <div class="round-step step-2"><img src="<?php print get_template_directory_uri(); ?>/library/images/pre-screen-icon.png" alt="Pre-Screening Committee" title="Pre-Screening Committee"/></div>
         <div class="process-line line-2"></div>      
-        <div class="round-step step-3">icon</div>
+        <div class="round-step step-3"><img src="<?php print get_template_directory_uri(); ?>/library/images/presentation-icon.png" alt="Presentation" title="Presentation"/></div>
         <div class="process-line line-3"></div>      
-        <div class="round-step step-4">icon</div>
+        <div class="round-step step-4"><img src="<?php print get_template_directory_uri(); ?>/library/images/diligence-icon.png" alt="Diligence" title="Diligence"/></div>
         <div class="process-line line-4"></div>      
-        <div class="round-step step-5">icon</div>
+        <div class="round-step step-5"><img src="<?php print get_template_directory_uri(); ?>/library/images/funding-icon.png" alt="Funding" title="Funding"/></div>
         <div class="process-line line-5"></div>      
-        <div class="round-step step-6">icon</div>  
+        <div class="round-step step-6"><img src="<?php print get_template_directory_uri(); ?>/library/images/growth-icon.png" alt="Growth" title="Growth"/></div>  
       </div>                            
     </div>  
   </div>
