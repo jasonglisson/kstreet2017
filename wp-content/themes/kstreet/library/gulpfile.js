10baaee5c20749e9e9f73198e8abcde066ec0ab7
Jason Glisson <jasonglisson@Jasons-MacBook-Air.local>
2017-06-22 22:49:04 -0400

    Added steps for process

--- wp-content/themes/kstreet/library/gulpfile.js
+++ wp-content/themes/kstreet/library/gulpfile.js
@@ -7,7 +7,7 @@ var	watch = require('gulp-watch');
 gulp.task('sass', function () {
   return gulp.src('scss/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('css'));
+    .pipe(gulp.dest('./css'));
 });
  
 gulp.task('watch', function () {
--- wp-content/themes/kstreet/page--homepage.php
+++ wp-content/themes/kstreet/page--homepage.php
@@ -6,29 +6,46 @@ Template Name: Homepage
 
 <?php get_header(); ?>
 			
-			<div id="content">
-			
-				<div id="inner-content" class="row">
-			
-				    <div id="main" class="large-12 medium-12 columns" role="main">
-					    	<h3 class="raleway"><?php the_field('homepage_heading'); ?></h3>    					
-    				</div> <!-- end #main -->
+<div id="content">
 
-				</div> <!-- end #inner-content -->
-<!-- 				<div class="overlay"></div> -->
-				<div class="arrow show-for-medium-up"></div>
-			</div> <!-- end #content -->
-			<div class="about-us">
-  			<div class="large-6 columns">what we do</div>
-  			<div class="large-6 columns" style="background: #ddd;">picture</div>
-  			<div class="large-6 columns" style="background: #ddd;">picture</div>
-  			<div class="large-6 columns">who we are</div>  			  			
-			</div>	
-			<div class="mailchimp-signup">
-				<div class="row">
-					<h3>K Street Newsletter Signup</h3>
-					<div class="header-line"></div>
-					<?php echo do_shortcode( '[mc4wp_form]' ); ?>					
-				</div>	
-			</div>
+  <div id="inner-content" class="row">
+    <div id="main" class="large-12 medium-12 columns" role="main">
+      <h3 class="raleway"><?php the_field('homepage_heading'); ?></h3>    					
+    </div> <!-- end #main -->
+  </div> <!-- end #inner-content -->
+	<div class="arrow show-for-medium-up"></div>
+</div> <!-- end #content -->
+<div class="about-us">
+	<div class="large-6 columns">what we do</div>
+	<div class="large-6 columns" style="background: #ddd;">picture</div>
+	<div class="large-6 columns" style="background: #ddd;">picture</div>
+	<div class="large-6 columns">who we are</div>  			  			
+</div>	
+<div class="process">
+  <div class="row">
+    <h2 class="large-12 columns">Investment Process</h2>
+    <div classs="large-12 columns">
+      <div class="process-wrap">
+        <div class="round-step step-1">icon</div>
+        <div class="process-line line-1"></div>
+        <div class="round-step step-2">icon</div>
+        <div class="process-line line-2"></div>      
+        <div class="round-step step-3">icon</div>
+        <div class="process-line line-3"></div>      
+        <div class="round-step step-4">icon</div>
+        <div class="process-line line-4"></div>      
+        <div class="round-step step-5">icon</div>
+        <div class="process-line line-5"></div>      
+        <div class="round-step step-6">icon</div>  
+      </div>                            
+    </div>  
+  </div>
+</div>  
+<div class="portfolio">
+  <div class="row">
+    <h2 class="large-12 columns">Portfolio</h2>
+  </div>
+</div> 
+  
+  
 <?php get_footer(); ?>
