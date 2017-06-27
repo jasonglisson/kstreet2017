11259324b6df5a2d0bba79c2b901d3f985a85c79
Jason Glisson <jasonglisson@Jasons-MacBook-Air.local>
2017-06-26 23:48:28 -0400

    added spinning effect to icons

--- wp-content/themes/kstreet/page--homepage.php
+++ wp-content/themes/kstreet/page--homepage.php
@@ -26,17 +26,17 @@ Template Name: Homepage
     <h1 class="large-12 columns">Investment Process</h1>
     <div classs="large-12 columns">
       <div class="process-wrap">
-        <div class="round-step step-1"><img src="<?php print get_template_directory_uri(); ?>/library/images/application-icon.png" alt="Application" title="Application"/></div>
+        <div class="round-step spin circle step-1"><img src="<?php print get_template_directory_uri(); ?>/library/images/application-icon.png" alt="Application" title="Application"/><span>1. Application</span></div>
         <div class="process-line line-1"></div>
-        <div class="round-step step-2"><img src="<?php print get_template_directory_uri(); ?>/library/images/pre-screen-icon.png" alt="Pre-Screening Committee" title="Pre-Screening Committee"/></div>
+        <div class="round-step spin circle step-2"><img src="<?php print get_template_directory_uri(); ?>/library/images/pre-screen-icon.png" alt="Pre-Screening Committee" title="Pre-Screening Committee"/></div>
         <div class="process-line line-2"></div>      
-        <div class="round-step step-3"><img src="<?php print get_template_directory_uri(); ?>/library/images/presentation-icon.png" alt="Presentation" title="Presentation"/></div>
+        <div class="round-step spin circle step-3"><img src="<?php print get_template_directory_uri(); ?>/library/images/presentation-icon.png" alt="Presentation" title="Presentation"/></div>
         <div class="process-line line-3"></div>      
-        <div class="round-step step-4"><img src="<?php print get_template_directory_uri(); ?>/library/images/diligence-icon.png" alt="Diligence" title="Diligence"/></div>
+        <div class="round-step spin circle step-4"><img src="<?php print get_template_directory_uri(); ?>/library/images/diligence-icon.png" alt="Diligence" title="Diligence"/></div>
         <div class="process-line line-4"></div>      
-        <div class="round-step step-5"><img src="<?php print get_template_directory_uri(); ?>/library/images/funding-icon.png" alt="Funding" title="Funding"/></div>
+        <div class="round-step spin circle step-5"><img src="<?php print get_template_directory_uri(); ?>/library/images/funding-icon.png" alt="Funding" title="Funding"/></div>
         <div class="process-line line-5"></div>      
-        <div class="round-step step-6"><img src="<?php print get_template_directory_uri(); ?>/library/images/growth-icon.png" alt="Growth" title="Growth"/></div>  
+        <div class="round-step spin circle step-6"><img src="<?php print get_template_directory_uri(); ?>/library/images/growth-icon.png" alt="Growth" title="Growth"/></div>  
       </div>                            
     </div>  
   </div>
