f582b0f38d3c90f6e259348f8d70638cefd71dea
Jason Glisson <jason.glisson.com>
2017-07-21 16:55:05 -0400

    worked on portfolio page

--- wp-content/themes/kstreet/page--investments.php
+++ wp-content/themes/kstreet/page--investments.php
@@ -30,35 +30,71 @@ Template Name: Investments
       ?>	
   	</div>	
 	  <div class="investments large-9 columns">
-    <?php 
-    
-    // args
-    $args = array(
-    	'numberposts'	=> -1,
-    	'post_type'		=> 'investment',
-    	'meta_key'		=> 'investment_category',
-    	'meta_value'	=> 'gov_tech'
-    );
-    
-    
-    // query
-    $the_query = new WP_Query( $args );
-    
-    ?>
-    <?php if( $the_query->have_posts() ): ?>
-    	<ul>
-    	<?php while( $the_query->have_posts() ) : $the_query->the_post(); ?>
-    		<li>
-    			<a href="<?php the_permalink(); ?>">
-    				<?php the_title(); ?>
-    			</a>
-    		</li>
-    	<?php endwhile; ?>
-    	</ul>
-    <?php endif; ?>
-    
-    <?php wp_reset_query();	 // Restore global post data stomped by the_post(). ?>
-
-	</div>
+  	  
+    	<?php
+      //$field = get_field_object('investment_category');
+      $field_key = "field_5971513da958f";
+      $field = get_field_object($field_key);
+      
+      if( $field ):
+    		foreach( $field['choices'] as $k => $v ):
+      		//print_r($field);
+    			echo '<h2 class="large-12 columns" id="'. $k . '">' . $v . '</h2>';
+    		
+    		  // args
+          $args = array(
+          	'numberposts'	=> -1,
+          	'post_type'		=> 'investment',
+          	'meta_key'		=> 'investment_category',
+          	'meta_value'	=> $k
+          );
+      		
+          // query
+          $the_query = new WP_Query( $args );
+          
+          ?>
+          <?php if( $the_query->have_posts() ): ?>
+          	<ul class="port-cat-list">
+          	<?php while( $the_query->have_posts() ) : $the_query->the_post(); ?>
+          		<li class="portfolio-item large-4 columns">
+        				<img src="<?php the_field('investment_image', $post->ID);?>">
+        				<div class="inner-port-wrap">
+          				<img src="<?php the_field('investment_image', $post->ID);?>">
+          				<h5><?php the_title(); ?> <a href="<?php the_field('link_to_company_site', $post->ID);?>" target="_blank">visit site</a></h5>
+          				<div class="blurb"><?php the_field('investment_blurb', $post->ID);?></div>
+        				</div>	
+          		</li>
+          	<?php endwhile; ?>
+          	</ul>
+          <?php endif; ?>
+          
+          <?php wp_reset_query();	 // Restore global post data stomped by the_post().       		
+          		
+         
+          	  
+  	  endforeach;
+      
+       endif;
+      // query
+     // $the_query = new WP_Query( $args );
+      
+/*
+      ?>
+      <?php if( $the_query->have_posts() ): ?>
+      	<ul>
+      	<?php while( $the_query->have_posts() ) : $the_query->the_post(); ?>
+      		<li>
+      			<a href="<?php the_permalink(); ?>">
+      				<?php the_title(); ?>
+      			</a>
+      		</li>
+      	<?php endwhile; ?>
+      	</ul>
+      <?php endif; ?>
+      
+      <?php wp_reset_query();	 // Restore global post data stomped by the_post(). 
+*/?>
+  
+  	</div>
 	</div>  
 <?php get_footer(); ?>
