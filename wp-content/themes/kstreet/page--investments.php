69c460b3eec762226a71c6ac4adac6bab0dcfabb
Jason Glisson <jasonglisson@Jasons-MacBook-Air-2.local>
2017-07-20 22:36:04 -0400

    Added portfolio page

--- wp-content/themes/kstreet/page--investments.php
+++ wp-content/themes/kstreet/page--investments.php
@@ -6,64 +6,59 @@ Template Name: Investments
 
 <?php get_header(); ?>
 			
-			<div class="top-title-wrap">
-				<div id="inner-content" class="row">
-				    <div id="main" class="large-12 columns first" role="main">
-						<h1 class="page-title"><?php echo get_the_title(); ?></h1>					
-				    </div> <!-- end #main -->
-				</div> <!-- end #inner-content -->
-				<div class="overlay"></div>
-			</div> <!-- end #content -->
-			<div class="investments">
-				<?php 
-				
-				$posts = get_field('investments');
-				
-				if( $posts ): ?>
-				    <div class="row">
-				    <?php foreach( $posts as $post): // variable must be called $post (IMPORTANT) ?>
-				        <?php setup_postdata($post); ?>
-				        <div class="large-4 columns invest">
-					        <?php $company_link = get_field('link_to_company_site', $post); 
-						        if(!empty($company_link)): ?>
-									<a href="<?php echo $company_link; ?>" title="<?php the_title(); ?>" target="_blank"><img src="<?php echo get_field('investment_image', $post); ?>"></a>
-							<?php else: ?>
-									<img src="<?php echo get_field('investment_image', $post); ?>">
-							<?php endif; ?>		
-				        </div>
-				    <?php endforeach; ?>
-				    </div>
-				    <?php wp_reset_postdata(); // IMPORTANT - reset the $post object so the rest of the page works correctly ?>
-				<?php endif; ?>
-			</div>
-			<div class="process-heading">
-				<div class="row">
-					<div class="large-12 columns">
-						<h3>Exits</h3>
-					</div>
-				</div>
-			</div>
-			<div class="investments">
-				<?php 
-				
-				$posts = get_field('exits');
-				
-				if( $posts ): ?>
-				    <div class="row">
-				    <?php foreach( $posts as $post): // variable must be called $post (IMPORTANT) ?>
-				        <?php setup_postdata($post); ?>
-				        <div class="large-4 columns invest">
-					        <?php $company_link = get_field('link_to_company_site', $post); 
-						        if(!empty($company_link)): ?>
-									<a href="<?php echo $company_link; ?>" title="<?php the_title(); ?>" target="_blank"><img src="<?php echo get_field('investment_image', $post); ?>"></a>
-							<?php else: ?>
-									<img src="<?php echo get_field('investment_image', $post); ?>">
-							<?php endif; ?>	
-				        </div>
-				    <?php endforeach; ?>
-				    </div>
-				    <?php wp_reset_postdata(); // IMPORTANT - reset the $post object so the rest of the page works correctly ?>
-				<?php endif; ?>
+	<div class="top-title-wrap">
+		<div id="inner-content" class="row">
+	    <div class="large-12 columns title-wrap first" role="main">
+			  <h1 class="page-title"><?php echo get_the_title(); ?></h1>					
+	    </div> <!-- end #main -->
+		</div> <!-- end #inner-content -->
+	</div> <!-- end #content -->
+	<div class="row">
+  	<div class="large-3 columns port-menu">
+    	<?php
+      //$field = get_field_object('investment_category');
+      $field_key = "field_5971513da958f";
+      $field = get_field_object($field_key);
+      
+      if( $field ):
+        '<ul>';
+      		foreach( $field['choices'] as $k => $v ) {
+      			echo '<li class=""><a href="#' . $k . '">' . $v . '</a></li>';
+      		}
+      	'</ul>';	
+      endif;
+      ?>	
+  	</div>	
+	  <div class="investments large-9 columns">
+    <?php 
+    
+    // args
+    $args = array(
+    	'numberposts'	=> -1,
+    	'post_type'		=> 'investment',
+    	'meta_key'		=> 'investment_category',
+    	'meta_value'	=> 'gov_tech'
+    );
+    
+    
+    // query
+    $the_query = new WP_Query( $args );
+    
+    ?>
+    <?php if( $the_query->have_posts() ): ?>
+    	<ul>
+    	<?php while( $the_query->have_posts() ) : $the_query->the_post(); ?>
+    		<li>
+    			<a href="<?php the_permalink(); ?>">
+    				<?php the_title(); ?>
+    			</a>
+    		</li>
+    	<?php endwhile; ?>
+    	</ul>
+    <?php endif; ?>
+    
+    <?php wp_reset_query();	 // Restore global post data stomped by the_post(). ?>
 
-			</div>
+	</div>
+	</div>  
 <?php get_footer(); ?>
